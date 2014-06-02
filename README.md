06b65cd1da3d4237a7514bbad50ee1b4358c7313
Guillaume Legrand <guillaume.legrand@payleven.de>
2014-06-02 11:11:35 +0200

    Add possibility to select the device to uninstall from
    through the '-s' option forwared to adb.
    
    Also redact a README.

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ adb_batch_uninstaller
 =====================
 
 Python script to batch uninstall a bunch of Android applications from a device using ADB.
+
+Typical use case is to have several applications to uninstall frequently and at the same time, e.g. an application and its test or sample application, or its different flavors.
+
+Settings
+--------
+
+Currently the applications to uninstall are set within in a constant at the head of the script, `UNINSTALL_APP_PREFIXES`.
+As the name suggests this is a prefix so any package which name *starts with* any these strings will be uninstalled.
+
+Usage
+-----
+
+The only option it currently accepts is
+- `-s <DEVICE>` which is directly passed to `adb` to specify the device to uninstall from if several are available. See `adb` documentation for details.
+
+Installation
+------------
+
+`python3` and `adb` are required and have to be on the `$PATH` of the executing shell.
+Then just make the script executable and run it:
+
+    $ chmod u+x ./adb_batch_uninstaller.py
+    $ ./adb_batch_uninstaller.py
+
--- adb_batch_uninstaller.py
+++ adb_batch_uninstaller.py
@@ -7,13 +7,21 @@ __author__ = 'Guillaume Legrand'
 
 UNINSTALL_APP_PREFIXES = ['com.example']
 
-CMD_ADB_LIST_PACKAGES = ['adb', 'shell', 'pm list packages -3 -f']
+CMD_ADB_NAME = 'adb'
+CMD_ADB_DEVICE_ARG_PREFIX = '-s'
+CMD_ADB_SHELL_COMMAND = ['shell', 'pm list packages -3 -f']
+CMD_ADB_UNINSTALL_COMMAND = ['uninstall']
 
 
-def adb_uninstall(package):
-    CMD_ADB_UNINSTALL_PACKAGE = ['adb', 'uninstall']
+def adb_uninstall(device, package):
+    uninstallPackageCmd = [CMD_ADB_NAME] + ([CMD_ADB_DEVICE_ARG_PREFIX, device] if device else []) + CMD_ADB_UNINSTALL_COMMAND
 
-    subprocess.check_call(CMD_ADB_UNINSTALL_PACKAGE + [package])
+    subprocess.check_call(uninstallPackageCmd + [package])
+
+
+def adb_list_packages(device):
+    listPackagesCmd = [CMD_ADB_NAME] + ([CMD_ADB_DEVICE_ARG_PREFIX, device] if device else []) + CMD_ADB_SHELL_COMMAND
+    return subprocess.check_output(listPackagesCmd)
 
 
 def get_package_adb(package_line):
@@ -21,10 +29,10 @@ def get_package_adb(package_line):
     return suffix
 
 
-def uninstall(app_prefixes):
+def uninstall(device, app_prefixes):
     installed_packages = ''
     try:
-        retrieved_packages = subprocess.check_output(CMD_ADB_LIST_PACKAGES)
+        retrieved_packages = adb_list_packages(device)
         installed_packages = (retrieved_packages.decode(sys.stdout.encoding)).splitlines()
         installed_packages = [get_package_adb(line) for line in installed_packages]
     except subprocess.CalledProcessError as calledProcessError:
@@ -39,11 +47,17 @@ def uninstall(app_prefixes):
 
     for package in packages_to_uninstall:
         try:
-            adb_uninstall(package)
+            adb_uninstall(device, package)
             print("uninstalled: " + package)
         except subprocess.CalledProcessError as calledProcessError:
             print("Failed execute adb to uninstall package '" + package + "'", calledProcessError, file=sys.stderr)
 
 
+def get_device_argument():
+    # no need for argparse yet
+    args = sys.argv
+    return args[1] if len(args) >= 2 else None
+
+
 if __name__ == '__main__':
-    uninstall(UNINSTALL_APP_PREFIXES)
+    uninstall(get_device_argument(), UNINSTALL_APP_PREFIXES)
